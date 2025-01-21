ae728771fd3f8ed9d623bb6f8fe25f9e38244487
Jeff. <jeffreysfu@gmail.com>
2025-01-21 18:18:51 -0400

    Afor suggestions implemented

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,6 +9,25 @@ export default {
             fontFamily: {
                 playwrite: ['PlayWrite', 'sans-serif'],
                 poppins: ['Poppins', 'sans-serif']
+            },
+            animation: {
+                slideDown: 'slideDown 2s ease-out forwards',
+                slideInLeft: 'slideInLeft 2s ease-out forwards',
+                slideInUp: 'slideInUp 2s ease-out forwards'
+            },
+            keyframes: {
+                slideDown: {
+                    '0%': { transform: 'translateY(-50px)', opacity: '0' },
+                    '100%': { transform: 'translateY(0)', opacity: '1' }
+                },
+                slideInLeft: {
+                    '0%': { transform: 'translateX(-100%)', opacity: '0' },
+                    '100%': { transform: 'translateX(0)', opacity: '1' }
+                },
+                slideInUp: {
+                    '0%': { transform: 'translateY(20px)', opacity: '0' },
+                    '100%': { transform: 'translateY(0)', opacity: '1' }
+                }
             }
         }
     },
